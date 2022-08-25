d5e1dc59c2e230f5fef059d4867bc8377a50fb48
www-data <www-data@bb15c25e1329>
2022-08-25 16:42:18 +0000

    update

---
+++ app/code/ISN/WebserviceCore/Model/Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ISN\WebserviceCore\Model;
+
+/**
+ * Contact module configuration
+ */
+class Test
+{
+}
