fe4901546aba7a55406e6ccf1c511683b590be49
www-data <www-data@bb15c25e1329>
2022-08-26 16:43:28 +0000

    init test

---
+++ app/code/ISN/WebserviceCore/Test/Unit/DummyTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace ISN\WebserviceCore\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testNothing()
+    {
+        $this->assertEquals(true, false);
+    }
+}
